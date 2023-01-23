0d924b8881b2f6c2743f18fda7a81af26d33e345
Mevlana Akcin <mevlanaakcin@prototipx.com>
2023-01-23 18:53:55 +0300

    Stm32xxx CD74HC4067 Output Example and Lib

---
+++ STM32xxxx/examples/CD74HC4067-Output/CD74HC4067/Core/Src/CD74HC4067.cpp
@@ -0,0 +1,58 @@
+/*
+ * CD74HC4067.cpp
+ *
+ *  Created on: Jan 23, 2023
+ *      Author: Mevlana
+ */
+
+#include "CD74HC4067.h"
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal.h"
+
+///////////////////////////////////////
+// Constructor & cd74hc4067
+CD74HC4067::CD74HC4067(int s0, int s1, int s2, int s3) {
+
+//	pinMode(s0, OUTPUT);
+//	pinMode(s1, OUTPUT);
+//	pinMode(s2, OUTPUT);
+//	pinMode(s3, OUTPUT);
+
+	setpincd(s0, s1, s2, s3);
+}
+// Constructor & cd74hc4067
+///////////////////////////////////////
+
+///////////////////////////////////////
+// SetpinCd
+void CD74HC4067::setpincd(int s0, int s1, int s2, int s3) {
+	pinCd[0] = s0;
+	pinCd[1] = s1;
+	pinCd[2] = s2;
+	pinCd[3] = s3;
+
+}
+// SetpinCd
+///////////////////////////////////////
+
+///////////////////////////////////////
+// setChannel
+void CD74HC4067::setChannel(GPIO_TypeDef *port, int ch) {
+	for (int i = 0; i < 4; i++) {
+		HAL_GPIO_WritePin(port, pinCd[i],
+				muxChannel[ch][i] == 1 ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	}
+}
+// setChannel
+///////////////////////////////////////
+
+///////////////////////////////////////
+// readChannel
+int CD74HC4067::readChannel(int pin) {
+	return 0;
+}
+// readChannel
+///////////////////////////////////////
+
---
+++ STM32xxxx/library/CD74HC4067.h
@@ -0,0 +1,42 @@
+/*
+ * CD74HC4067.h
+ *
+ *  Created on: Jan 23, 2023
+ *      Author: Mevlana
+ */
+
+#ifndef INC_CD74HC4067_H_
+#define INC_CD74HC4067_H_
+#include "stm32f4xx_hal.h"
+
+class CD74HC4067
+{
+private:
+    const int muxChannel[16][4] = {
+        {0, 0, 0, 0}, // channel 0
+        {1, 0, 0, 0}, // channel 1
+        {0, 1, 0, 0}, // channel 2
+        {1, 1, 0, 0}, // channel 3
+        {0, 0, 1, 0}, // channel 4
+        {1, 0, 1, 0}, // channel 5
+        {0, 1, 1, 0}, // channel 6
+        {1, 1, 1, 0}, // channel 7
+        {0, 0, 0, 1}, // channel 8
+        {1, 0, 0, 1}, // channel 9
+        {0, 1, 0, 1}, // channel 10
+        {1, 1, 0, 1}, // channel 11
+        {0, 0, 1, 1}, // channel 12
+        {1, 0, 1, 1}, // channel 13
+        {0, 1, 1, 1}, // channel 14
+        {1, 1, 1, 1}  // channel 15
+    };
+    int pinCd[4];
+    void setpincd(int, int, int, int);
+
+public:
+    CD74HC4067(int, int, int, int);
+    void setChannel(GPIO_TypeDef *, int);
+    int readChannel(int);
+};
+
+#endif /* INC_CD74HC4067_H_ */
